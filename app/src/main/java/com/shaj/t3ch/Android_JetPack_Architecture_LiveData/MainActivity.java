cd37e94616060a87ef717a3753458f4926e5c2c3
SHAJ <shakilahmedshaj@gmail.com>
2019-01-07 17:52:42 +0600

    Before LiveData

---
+++ app/src/main/java/com/shaj/t3ch/Android_JetPack_Architecture_LiveData/MainActivity.java
@@ -0,0 +1,41 @@
+package com.shaj.t3ch.Android_JetPack_Architecture_LiveData;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = this.getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        TextView mTextView = findViewById(R.id.tvNumber);
+//        MainActivityDataGenerator myData = new MainActivityDataGenerator();
+        MainActivityViewModel model = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+        String myRandomNumber = model.getNumber();
+        mTextView.setText(myRandomNumber);
+
+        Log.i(TAG, "Random Number Set");
+    }
+}
